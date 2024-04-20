53fed2cca7961a4e97b221741c06c14c5685a95e
Beschieru Marius <marius_beschieru@yahoo.com>
2024-04-20 23:44:14 +0300

    Map exemple

--- src/modules/DeleteVPN/DeleteVpn.tsx
+++ src/modules/DeleteVPN/DeleteVpn.tsx
@@ -1,8 +1,21 @@
 ﻿import React, { useState } from 'react';
 import {
-  DeleteVpnContainer, DeleteVpnTitle, UUIDForm, UUIDLabel, UUIDInput, SubmitButton, ResponseSection, ResponseItem, ResponseLabel,
-  ResponseValue, ResponseContent, ResponseBox, RequestResponseLabel, CopyButton
+  DeleteVpnContainer,
+  DeleteVpnTitle,
+  UUIDForm,
+  UUIDLabel,
+  UUIDInput,
+  SubmitButton,
+  ResponseSection,
+  ResponseItem,
+  ResponseLabel,
+  ResponseValue,
+  ResponseContent,
+  ResponseBox,
+  RequestResponseLabel,
+  CopyButton,
 } from './styles';
+import { IResponseItem, createResponseItems } from './constants';
 
 const copyToClipboard = async (text: string) => {
   if (!navigator.clipboard) {
@@ -18,51 +31,44 @@ const copyToClipboard = async (text: string) => {
 };
 
 const DeleteVpn = (): JSX.Element => {
-  const [uuid, setUuid] = useState('');
-  const [submitted, setSubmitted] = useState(false);
+  const [uuid, setUuid] = useState<string>('');
+  const [submitted, setSubmitted] = useState<boolean>(false);
+  const [responseItems, setResponseItems] = useState<IResponseItem[]>([]);
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    const newResponseItems = createResponseItems(uuid, '200 Success');
+    setResponseItems(newResponseItems);
     setSubmitted(true);
   };
   return (
     <DeleteVpnContainer>
-      <DeleteVpnTitle><b>Delete VPN</b></DeleteVpnTitle>
+      <DeleteVpnTitle>
+        <b>Delete VPN</b>
+      </DeleteVpnTitle>
       <UUIDForm onSubmit={handleSubmit}>
         <UUIDLabel htmlFor="uuid">UUID</UUIDLabel>
-        <UUIDInput
-          id="uuid"
-          type="text"
-          value={uuid}
-          onChange={(e) => setUuid(e.target.value)}
-          placeholder="00000000-0000-0000-0000-000000000000"
-          required
-        />
+        <UUIDInput id="uuid" type="text" value={uuid} onChange={e => setUuid(e.target.value)} placeholder="00000000-0000-0000-0000-000000000000" required />
         <SubmitButton type="submit">Submit</SubmitButton>
       </UUIDForm>
       {submitted && (
-  <ResponseSection>
-    <RequestResponseLabel>Request response</RequestResponseLabel>
-    <ResponseLabel>UUID</ResponseLabel>
-    <ResponseItem>
-      <ResponseBox>
-        <ResponseContent>
-          <ResponseValue>{uuid || 'None provided'}</ResponseValue>
-          <CopyButton onClick={() => copyToClipboard(uuid || 'None provided')}>Copy</CopyButton>
-        </ResponseContent>
-      </ResponseBox>
-    </ResponseItem>
-    <ResponseLabel>Status code</ResponseLabel>
-    <ResponseItem>
-      <ResponseBox>
-        <ResponseContent>
-          <ResponseValue>200 Success</ResponseValue>
-          <CopyButton onClick={() => copyToClipboard('200 Success')}>Copy</CopyButton>
-        </ResponseContent>
-      </ResponseBox>
-    </ResponseItem>
-  </ResponseSection>
-)}
+        <ResponseSection>
+          <RequestResponseLabel>Request response</RequestResponseLabel>
+          {responseItems.map((item, index) => (
+            <>
+              <ResponseLabel>{item.title}</ResponseLabel>
+              <ResponseItem key={index}>
+                <ResponseBox>
+                  <ResponseContent>
+                    <ResponseValue>{item.value}</ResponseValue>
+                    <CopyButton onClick={() => copyToClipboard(item.value)}>Copy</CopyButton>
+                  </ResponseContent>
+                </ResponseBox>
+              </ResponseItem>
+            </>
+          ))}
+        </ResponseSection>
+      )}
     </DeleteVpnContainer>
   );
 };
