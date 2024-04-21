04c99f283198a2f0cc23aa6016c1c9f0f4e42cd2
maria <maria@yahoo.com>
2024-04-21 23:23:21 +0300

    fixed samba delete form

--- src/modules/DeleteSamba/DeleteSamba.tsx
+++ src/modules/DeleteSamba/DeleteSamba.tsx
@@ -1,8 +1,23 @@
 import React, { useState } from 'react';
 import {
-  DeleteSambaContainer, DeleteSambaTitle, UUIDForm, UUIDLabel, UUIDInput, SubmitButton, ResponseSection, ResponseItem, ResponseLabel,
-  ResponseValue, ResponseContent, ResponseBox, RequestResponseLabel, CopyButton
+  DeleteSambaTitle,
+  UUIDForm,
+  UUIDLabel,
+  UUIDInput,
+  SubmitButton,
+  ResponseSection,
+  ResponseLabel,
+  ResponseValue,
+  RequestResponseLabel,
+  CopyButton,
+  SambaDeleteContainer,
+  SambaDeleteInnerContainer,
+  UuidInputWrapper,
+  SambaDeleteResponseWrapper,
+  SambaDeleteResponseItem,
+  SambaDeleteResponseBox,
 } from './styles';
+import { IResponseItem, createResponseItems } from './constants';
 
 const copyToClipboard = async (text: string) => {
   if (!navigator.clipboard) {
@@ -11,50 +26,59 @@ const copyToClipboard = async (text: string) => {
   }
   try {
     await navigator.clipboard.writeText(text);
-    alert('Text copied to clipboard');
   } catch (err) {
     console.error('Failed to copy text: ', err);
   }
 };
 
 const DeleteSamba = (): JSX.Element => {
-  const [uuid, setUuid] = useState('');
-  const [submitted, setSubmitted] = useState(false);
+  const [uuid, setUuid] = useState<string>('');
+  const [isFormSubmitted, setIsFormSubmitted] = useState<boolean>(false);
+  const [responseItems, setResponseItems] = useState<IResponseItem[]>([]);
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    setSubmitted(true);
+    const newResponseItems = createResponseItems(uuid, '200 Success');
+    setResponseItems(newResponseItems);
+    setIsFormSubmitted(true);
   };
+
+  const handleValueSize = (value: string): string => {
+    if (value.length > 50) {
+      return `${value.slice(0, 25)}...`;
+    }
+    return value;
+  };
+
   return (
-    <DeleteSambaContainer>
-      <DeleteSambaTitle><b>Delete Samba</b></DeleteSambaTitle>
-      <UUIDForm onSubmit={handleSubmit}>
-        <UUIDLabel htmlFor="uuid">UUID</UUIDLabel>
-        <UUIDInput
-          id="uuid"
-          type="text"
-          value={uuid}
-          onChange={(e) => setUuid(e.target.value)}
-          placeholder="00000000-0000-0000-0000-000000000000"
-          required
-        />
-        <SubmitButton type="submit">Submit</SubmitButton>
-      </UUIDForm>
-      {submitted && (
-  <ResponseSection>
-    <RequestResponseLabel>Request response</RequestResponseLabel>
-    <ResponseLabel>Status code</ResponseLabel>
-    <ResponseItem>
-      <ResponseBox>
-        <ResponseContent>
-          <ResponseValue>200 Success</ResponseValue>
-          <CopyButton onClick={() => copyToClipboard('200 Success')}>Copy</CopyButton>
-        </ResponseContent>
-      </ResponseBox>
-    </ResponseItem>
-  </ResponseSection>
-)}
-    </DeleteSambaContainer>
+    <SambaDeleteContainer>
+      <SambaDeleteInnerContainer>
+        <DeleteSambaTitle>Delete Samba</DeleteSambaTitle>
+        <UUIDForm onSubmit={handleSubmit}>
+          <UuidInputWrapper>
+            <UUIDLabel htmlFor="uuid">UUID</UUIDLabel>
+            <UUIDInput id="uuid" type="text" value={uuid} onChange={e => setUuid(e.target.value)} placeholder="00000000-0000-0000-0000-000000000000" required />
+          </UuidInputWrapper>
+          <SubmitButton type="submit">Submit</SubmitButton>
+        </UUIDForm>
+        {isFormSubmitted && (
+          <ResponseSection>
+            <RequestResponseLabel>Request response</RequestResponseLabel>
+            <SambaDeleteResponseWrapper>
+              {responseItems.map(item => (
+                <SambaDeleteResponseItem>
+                  <ResponseLabel>{item.title}</ResponseLabel>
+                  <SambaDeleteResponseBox>
+                    <ResponseValue>{handleValueSize(item.value)}</ResponseValue>
+                    <CopyButton onClick={() => copyToClipboard(item.value)}>Copy</CopyButton>
+                  </SambaDeleteResponseBox>
+                </SambaDeleteResponseItem>
+              ))}
+            </SambaDeleteResponseWrapper>
+          </ResponseSection>
+        )}
+      </SambaDeleteInnerContainer>
+    </SambaDeleteContainer>
   );
 };
 
