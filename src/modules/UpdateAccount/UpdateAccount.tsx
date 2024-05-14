a93bcccb11cd094a5d741f43ea43fd6a8a62c2d5
d3nis01 <denis.deaconu2003@gmail.com>
2024-05-14 18:09:45 +0300

    Added confirm password to update account

--- src/modules/UpdateAccount/UpdateAccount.tsx
+++ src/modules/UpdateAccount/UpdateAccount.tsx
@@ -38,24 +38,36 @@ const UpdateAccount = (): JSX.Element => {
   const [id, setId] = useState<string>('');
   const [newUsername, setNewUsername] = useState<string>('');
   const [newPassword, setNewPassword] = useState<string>('');
+  const [confirmNewPassword, setConfirmNewPassword] = useState<string>('');
   const [newEmail, setNewEmail] = useState<string>('');
   const [response, setResponse] = useState<any>(null);
   const [isUuidError, setIsUuidError] = useState<string>('');
+  const [confirmPasswordError, setConfirmPasswordError] = useState<string>('');
   const [isFormSubmitted, setIsFormSubmitted] = useState<boolean>(false);
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
+    if (!isUUID(id)) {
+      setIsUuidError('Invalid UUID!');
+      return;
+    } else {
+      setIsUuidError('');
+    }
+
+    if (newPassword !== confirmNewPassword) {
+      setConfirmPasswordError('Passwords do not match!');
+      return;
+    } else {
+      setConfirmPasswordError('');
+    }
+
     try {
       const updateData = {
         newUsername,
         newPassword,
         newEmail,
       };
-
-      if (!isUUID(id)) {
-        setIsUuidError('Invalid UUID!');
-        return;
-      }
 
       const updateResponse = await updateAccount(id, updateData);
       setResponse(updateResponse);
@@ -73,15 +85,28 @@ const UpdateAccount = (): JSX.Element => {
           <AccountUpdateInputWrapper>
             <AccountUpdateLabel htmlFor="uuid">UUID</AccountUpdateLabel>
             <AccountUpdateInput type="text" id="uuid" name="uuid" value={id} onChange={e => setId(e.target.value)} placeholder="" required />
-            {isUuidError && <AccountInputError>Invalid UUID</AccountInputError>}
+            {isUuidError && <AccountInputError>{isUuidError}</AccountInputError>}
           </AccountUpdateInputWrapper>
           <AccountUpdateInputWrapper>
             <AccountUpdateLabel htmlFor="username">New Username</AccountUpdateLabel>
             <AccountUpdateInput type="text" id="username" name="username" value={newUsername} onChange={e => setNewUsername(e.target.value)} placeholder="" required />
           </AccountUpdateInputWrapper>
           <AccountUpdateInputWrapper>
-            <AccountUpdateLabel htmlFor="password"> New Password</AccountUpdateLabel>
+            <AccountUpdateLabel htmlFor="password">New Password</AccountUpdateLabel>
             <AccountUpdateInput type="password" id="password" name="password" value={newPassword} onChange={e => setNewPassword(e.target.value)} placeholder="" required />
+          </AccountUpdateInputWrapper>
+          <AccountUpdateInputWrapper>
+            <AccountUpdateLabel htmlFor="confirmPassword">Confirm New Password</AccountUpdateLabel>
+            <AccountUpdateInput
+              type="password"
+              id="confirmPassword"
+              name="confirmPassword"
+              value={confirmNewPassword}
+              onChange={e => setConfirmNewPassword(e.target.value)}
+              placeholder=""
+              required
+            />
+            {confirmPasswordError && <AccountInputError>{confirmPasswordError}</AccountInputError>}
           </AccountUpdateInputWrapper>
           <AccountUpdateInputWrapper>
             <AccountUpdateLabel htmlFor="email">New Email</AccountUpdateLabel>
